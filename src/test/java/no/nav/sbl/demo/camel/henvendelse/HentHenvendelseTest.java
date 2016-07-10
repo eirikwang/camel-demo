731ae746fff851ec39701eb10e79c86c1135de94
eirik wang <eirilwan@gmail.com>
2016-07-10 08:37:31 +0200

    Lagt på dummy test

---
+++ src/test/java/no/nav/sbl/demo/camel/henvendelse/HentHenvendelseTest.java
@@ -0,0 +1,13 @@
+package no.nav.sbl.demo.camel.henvendelse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HentHenvendelseTest {
+    @Test
+    public void shouldTest(){
+
+    }
+
+}
